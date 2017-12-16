65936aa706d640b5f68e276a0f3ce453eb57f13a
Lukas0610 <mail@lukasberger.at>
2017-12-16 17:40:46 +0100

    releasetools: disable Brotli-compression
    Change-Id: Ib555bfc33f2afa9a07da105f024e5ee02a8e680e

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1583,25 +1583,8 @@ class BlockDifference(object):
     #   compressed_size:    942M | 869M (~8% reduced) | 854M
     #   compression_time:   75s  | 265s               | 719s
     #   decompression_time: 15s  | 25s                | 25s
-
-    if not self.src:
-      bro_cmd = ['bro', '--quality', '6',
-                 '--input', '{}.new.dat'.format(self.path),
-                 '--output', '{}.new.dat.br'.format(self.path)]
-      print("Compressing {}.new.dat with brotli".format(self.partition))
-      p = Run(bro_cmd, stdout=subprocess.PIPE)
-      p.communicate()
-      assert p.returncode == 0,\
-          'compression of {}.new.dat failed'.format(self.partition)
-
-      new_data_name = '{}.new.dat.br'.format(self.partition)
-      ZipWrite(output_zip,
-               '{}.new.dat.br'.format(self.path),
-               new_data_name,
-               compress_type=zipfile.ZIP_STORED)
-    else:
-      new_data_name = '{}.new.dat'.format(self.partition)
-      ZipWrite(output_zip, '{}.new.dat'.format(self.path), new_data_name)
+    new_data_name = '{}.new.dat'.format(self.partition)
+    ZipWrite(output_zip, '{}.new.dat'.format(self.path), new_data_name)
 
     ZipWrite(output_zip,
              '{}.patch.dat'.format(self.path),
