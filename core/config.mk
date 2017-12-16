62eb1e50a8103d029b90c3ef848f0e1067431df3
Lukas0610 <mail@lukasberger.at>
2017-12-16 21:32:08 +0100

    core: directly include NexusOS-vendor repo
    Change-Id: I838c7459c674be3e71e5ec62be8206f0f00007e7

--- core/config.mk
+++ core/config.mk
@@ -190,6 +190,7 @@ $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/ril)
 
 -include vendor/extra/BoardConfigExtra.mk
 -include vendor/lineage/config/BoardConfigLineage.mk
+-include vendor/nexus/board.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:
